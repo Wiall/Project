c49eda8d57aef7a2096eeffbee5419d07f6fbd40
Artur Serkeli <serkeli2006@gmail.com>
2025-04-20 20:22:46 +0300

    Added home page with animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About/About";
 import GameBoardLayout from "./pages/Gameboard/GameBoardLayout";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<GameBoardLayout />} />
         <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+import Stars from "../../components/Stars";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <Stars />
+      <div className="overlay">
+        <header className="hero">
+          <h1 className="title-glow">Echoes of Darkness</h1>
+          <p className="text-fade ">Forge your destiny in a world where magic meets shadows.</p>
+          <button className="cta-button " onClick={() => navigate("/")}>
+            Enter the Realm
+          </button>
+        </header>
+      </div>
+    </div>
+  );
+}
