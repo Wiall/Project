dac3b8cf0bb626e365f239f21a9f0b64b39945d7
Artur Serkeli <serkeli2006@gmail.com>
2025-04-21 18:56:07 +0300

    added game hub page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About/About";
 import GameBoardLayout from "./pages/Gameboard/GameBoardLayout";
 import Home from "./pages/Home/Home";
+import PlayerHomePage from "./pages/PlayerHub/PlayerHomePage";
+import DeckBuilderPage from "./pages/DeckBuilderPage/DeckBuilderPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<GameBoardLayout />} />
+        <Route path="/game-room" element={<GameBoardLayout />} />
+        <Route path="/game-page" element={<PlayerHomePage />} />
+        <Route path="/game-deck" element={<DeckBuilderPage />} />
         <Route path="/about" element={<About />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
